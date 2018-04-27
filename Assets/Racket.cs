1ee4b6adb1223f8ceb931e8be49505ac46e290ed
Sarias <53831285s@iespoblenou.org>
2018-04-27 15:53:08 +0200

    first commit

---
+++ Assets/Racket.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+    public float speed = 150.0f;
+
+
+    void FixedUpdate()
+    {
+        float horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * horizontalDirection * speed;
+    }
+}
